aad8670ab964e2fc5f405b437dc9592db99ecff6
Jools <jools@joomla.org>
2011-11-04 15:44:10 -0500

    Adding missing file - JGithubRefs class

---
+++ platform/libraries/joomla/client/github/githubrefs.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ * @package     Joomla.Platform
+ * @subpackage  Client
+ *
+ * @copyright   Copyright (C) 2005 - 2011 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE
+ */
+
+defined('JPATH_PLATFORM') or die;
+
+/**
+ * HTTP client class.
+ *
+ * @package     Joomla.Platform
+ * @subpackage  Client
+ * @since       11.1
+ */
+class JGithubRefs
+{
+	/**
+	 * Github Connector
+	 *
+	 * @var    JGithub
+	 * @since  11.3
+	 */
+	protected $connector = null;
+
+	/**
+	 * Constructor.
+	 *
+	 * @param   array  $options  Array of configuration options for the client.
+	 *
+	 * @return  void
+	 *
+	 * @since   11.1
+	 */
+	public function __construct($connector, $options = array())
+	{
+		$this->connector = $connector;
+	}
+
+	public function get($user, $repo, $ref)
+	{
+		return $this->connector->sendRequest('/repos/' . $user . '/' . $repo . '/git/refs/' . $ref)->body;
+	}
+}
